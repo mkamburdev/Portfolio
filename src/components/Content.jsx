729c5d5b80a03ea8aa3038a55f61527451a24799
Muhammed Kambur <muhammed.kambur@dci-student.org>
2024-07-09 11:09:56 +0200

    Update content in Content component to reflect IT experience and training

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,15 +4,15 @@ export default function Content() {
 	return (
 		<div className="w-11/12 m-auto">
 			<p className="text-secondary mt-8 py-3">
-				I have 3 years of experience as a web developer and I am currently a
-				full-time student in the further education program at{' '}
-				<span className="text-primary">DCI</span> to advance my skills as a{' '}
-				<span className="text-primary">full-stack developer</span>. The
-				intensive training program mainly focuses on the{' '}
-				<span className="text-primary">MERN stack</span>. I am passionate about
-				using my skills and experience to create value for organizations. With
-				my education and experience, I am confident in delivering
-				action-oriented insights for clients.
+				With about <span className="text-primary">7 years</span> of IT
+				experience behind me, I have focused on developing myself daily.
+				Currently, I am undergoing a{' '}
+				<span className="text-primary">full-time</span> training at{' '}
+				<span className="text-primary">DCI</span>. I am looking for a suitable
+				professional opportunity to work in a team and combine my previous IT
+				experience with newly acquired Full-Stack skills to advance on my path
+				to becoming a fully-fledged{' '}
+				<span className="text-primary">Full Stack Developer</span>.
 			</p>
 		</div>
 	)
